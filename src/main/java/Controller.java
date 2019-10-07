fc0da6d0f602795567ebbb9f6f4e0c6b219bff3a
chwostian <chwostian@post.pl>
2019-10-07 15:53:14 +0200

    Second form + css

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -16,9 +14,6 @@ import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.AudioClip;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -152,7 +147,8 @@ public class Controller extends Window {
 
     public void buttonClicked(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setHeaderText(actionEvent.getSource().toString());
+        alert.setHeaderText(actionEvent.getSource().toString() + " " + actionEvent.getEventType().getSuperType());
         alert.show();
+
     }
 }
