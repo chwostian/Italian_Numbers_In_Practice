122376db5ee677193faab0c88668fa5570286001
chwostian <chwostian@post.pl>
2019-10-09 20:33:50 +0200

    LearningController - before making changes

---
+++ src/main/java/Randomize.java
@@ -0,0 +1,55 @@
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Randomize {
+    private Long randomLong;
+    private Boolean numberHasBeenPlayed;
+    private Boolean answerWasCorrect;
+
+    public Randomize() {
+    }
+
+    public Randomize(Long lBound, Long uBound) {
+        this.randomLong = ThreadLocalRandom.current().nextLong(lBound,uBound);
+        this.numberHasBeenPlayed = false;
+    }
+
+    public Long getRandomLong() {
+        return randomLong;
+    }
+
+    public void setRandomLong(Long lBound, Long uBound) {
+
+        this.randomLong = ThreadLocalRandom.current().nextLong(lBound,uBound);
+    }
+
+    public Boolean getNumberHasBeenPlayed() {
+        return numberHasBeenPlayed;
+    }
+
+    public void setNumberHasBeenPlayed(Boolean numberHasBeenPlayed) {
+        this.numberHasBeenPlayed = numberHasBeenPlayed;
+    }
+
+    public Boolean getAnswerWasCorrect() {
+        return answerWasCorrect;
+    }
+
+    public void setAnswerWasCorrect(Boolean answerWasCorrect) {
+        this.answerWasCorrect = answerWasCorrect;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Randomize that = (Randomize) o;
+        return randomLong.equals(that.randomLong);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(randomLong);
+    }
+}
--- src/main/java/SettingController.java
+++ src/main/java/SettingController.java
@@ -1,8 +1,11 @@
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,7 +23,7 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 
-public class Controller extends Window {
+public class SettingController extends Window {
 
     @FXML
     private TextField txtPath;
@@ -34,6 +37,8 @@ public class Controller extends Window {
     @FXML
     private TextField txtPlaybackSpeed;
 
+    @FXML TextField txtField;
+
     @FXML
     private ResourceBundle resources;
 
@@ -41,7 +46,7 @@ public class Controller extends Window {
     private URL location;
 
     @FXML
-    private AnchorPane mainForm;
+    public AnchorPane mainForm;
 
     @FXML
     private ToggleGroup groupLevelOfCommand;
@@ -52,8 +57,15 @@ public class Controller extends Window {
     @FXML
     private Button btBackspace;
 
+    @FXML private Button btClear;
+
     @FXML
     private AudioClip audioClip;
+
+    @FXML
+    private Button btPlay;
+
+    public static HashSet<Randomize>  luckyNumbers = new HashSet();
 
     @FXML
     void showMessage(MouseEvent event) {
@@ -65,6 +77,7 @@ public class Controller extends Window {
 
     @FXML
     void openSecondForm(ActionEvent event) throws IOException, URISyntaxException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SecondForm.fxml"));
         Parent root1 = fxmlLoader.load();
         Stage stage = new Stage();
@@ -74,31 +87,6 @@ public class Controller extends Window {
         stage.setScene(new Scene(root1));
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.show();
-        StringBuilder str = new StringBuilder();
-        URL path = null;
-
-        for (int i=1; i<4; i++){
-            str = new StringBuilder();
-            str.append("src/main/resources/media/");
-            str.append(i);
-            str.append("00");
-            str.append(".mp3");
-
-            path = new File(str.toString()).toURI().toURL();
-
-            audioClip = new AudioClip(path.toString());
-            audioClip.play();
-            while (audioClip.isPlaying()) {
-                ;
-            }
-
-        }
-
-
-//        mediaView.setMediaPlayer(mediaPlayer);
-
-
-
 
 
 
@@ -127,7 +115,7 @@ public class Controller extends Window {
                 break;
             case "Advanced":
                 txtFrom.setText(String.valueOf(0));
-                txtTo.setText("INFINITY");
+                txtTo.setText("1000000000000");
                 txtPlaybackSpeed.setText("100%");
                 break;
 
@@ -146,9 +134,77 @@ public class Controller extends Window {
 
 
     public void buttonClicked(ActionEvent actionEvent) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setHeaderText(actionEvent.getSource().toString() + " " + actionEvent.getEventType().getSuperType());
-        alert.show();
 
+        Button bt = (Button) actionEvent.getSource();
+
+        if (isNumeric(bt.getText())) {
+            txtField.setText(txtField.getText()+bt.getText());
+        } else {
+            switch (bt.getId()) {
+                case "btBackspace":
+                    txtField.setText(txtField.getText(0, Math.max(0, txtField.getLength() - 1)));
+                    break;
+                case "btClear":
+                    txtField.clear();
+                    break;
+                case "btStart":
+                    bt.setDisable(true);
+                    startLearning(Long.parseLong(txtFrom.getText()), Long.parseLong(txtTo.getText()));
+                    break;
+                case "btPlay":
+                    luckyNumbers.stream().forEach(s->System.out.println(s.getRandomLong()));
+                    Optional<Randomize> firstResult = luckyNumbers.stream().filter(s->s.getNumberHasBeenPlayed().equals(false)).findFirst();
+                    if (firstResult.isPresent()) {
+                        firstResult.get().setNumberHasBeenPlayed(true);
+                        System.out.println(firstResult.get().getRandomLong());
+                    } else {
+                        Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                        newAlert.setHeaderText("Skończyły się liczby");
+                        newAlert.show();
+
+                    }
+                    break;
+            }
+
+
+    }
+    }
+
+    private void startLearning(Long lBound, Long uBound) {
+        while (luckyNumbers.size()<20) {
+            luckyNumbers.add(new Randomize(lBound, uBound));
+        }
+
+
+    }
+
+    private static boolean isNumeric(String txt) {
+        try {Long longFromString = Long.parseLong(txt);}
+        catch (NumberFormatException e) {return false;}
+        return true;
+    }
+
+
+
+    private void playNumbers(Integer number) throws MalformedURLException {
+        StringBuilder str = new StringBuilder();
+        URL path = null;
+
+        for (int i=1; i<4; i++){
+            str = new StringBuilder();
+            str.append("src/main/resources/media/");
+            str.append(i);
+            str.append("00");
+            str.append(".mp3");
+
+            path = new File(str.toString()).toURI().toURL();
+
+            audioClip = new AudioClip(path.toString());
+            audioClip.play();
+            while (audioClip.isPlaying()) {
+                ;
+            }
+
+        }
     }
 }
